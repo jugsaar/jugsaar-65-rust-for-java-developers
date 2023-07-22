bc588947b00e38a290e5c1a996953fdbc88f7992
Thomas Darimont <thomas.darimont@codecentric.de>
2023-07-22 10:39:12 +0200

    Add trait example

---
+++ ex025-traits/src/main.rs
@@ -0,0 +1,24 @@
+// "Interface" with generic type parameter Item
+trait MyTrait {
+    type Item;
+
+    fn create_item() -> Self::Item;
+}
+
+// Empty struct
+struct MyStruct;
+
+// We implement MyTrate for MyStruct
+impl MyTrait for MyStruct {
+    type Item = i32;
+
+    fn create_item() -> Self::Item {
+        // Self => Typ of implementing Struct
+        42
+    }
+}
+
+fn main() {
+    let item: i32 = MyStruct::create_item();
+    println!("Item: {}", item); // Output: Item: 42
+}
