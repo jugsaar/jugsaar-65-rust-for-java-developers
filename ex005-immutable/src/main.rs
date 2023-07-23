b39c17ce6fbe08eb592643adebf2c7aa699f9139
Thomas Darimont <thomas.darimont@codecentric.de>
2023-07-23 18:24:49 +0200

    Ease demoing

--- ex005-immutable/src/main.rs
+++ ex005-immutable/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let x = 13;
-    x = 32; // ERROR cannot assign twice to immutable variable `x`
+    // x = 32; // ERROR cannot assign twice to immutable variable `x`
 
     println!("x: {}", x);
 }
